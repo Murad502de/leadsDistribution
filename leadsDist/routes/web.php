ee9562433e768c5235b85873af837d5cedb0d525
Murad Rasulov <rassulovmurad@gmail.com>
2021-07-11 20:16:33 +0200

    add keys page

--- leadsDist/routes/web.php
+++ leadsDist/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get( '/privacyPolicy', function(){
+Route::get( '/privacyPolicy', function () {
     return view( 'privacyPolicy' );
+} );
+
+Route::get( '/keys', function () {
+    return 'Testseite';
 } );
