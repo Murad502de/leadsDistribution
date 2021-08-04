0c47de535bed605173df8683b7c131c253ccecaf
Мурад Расулов <rassulovmurad@gmail.com>
2021-08-05 00:21:03 +0200

    test

--- leadsDist/app/Http/Controllers/Api/DistributionController.php
+++ leadsDist/app/Http/Controllers/Api/DistributionController.php
@@ -78,7 +78,7 @@ class DistributionController extends Controller
 
         $task = new Task( $accountRequestData );
 
-        $responsibleUserIdEntity = 4444444;//7001125;
+        $responsibleUserIdEntity = 7001125;
         $query = 'filter[responsible_user_id][]=' . $responsibleUserIdEntity;
 
         $tasks = $task->getByQuery( $query );
